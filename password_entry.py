94e31eb734c1637120e7bf9b430ad97b3f990f09
LachlanGillard <lachlan.gillard@my.jcu.edu.au>
2019-08-20 09:29:24 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,9 @@
 """
 Lachlan Gillard
 """
+password = input("Password: ")
+while len(password) < 6:
+    print("Invalid Length")
+    password = input("Password: ")
+asteriks = '*' * len(password)
+print(asteriks)
